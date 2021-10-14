77830c0df6382e40b0dbfaba46f0023474c0325b
joyantabasak13 <joyantabasak13@gmail.com>
2021-10-14 08:28:00 +0600

    added papers
    Signed-off-by: joyantabasak13 <joyantabasak13@gmail.com>

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,10 @@
 import os
 import open3d as o3d
+import numpy as np
 
 if __name__ == '__main__':
     data_path = os.path.abspath("../data/bunny_normalized.ply")
     cloud = o3d.io.read_point_cloud(data_path) # Read the point cloud
-    o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud
+    
+    #o3d.visualization.draw_geometries([cloud], point_show_normal=True) # Visualize the point cloud
+
