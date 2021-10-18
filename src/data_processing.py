9b74e17d49c671e037101ab092acd34ab9a3447d
joyanta <joyantabasak13@gmail.com>
2021-10-18 14:19:27 +0600

    Added shapenet ex 1 aeroplane
    Signed-off-by: joyanta <joyantabasak13@gmail.com>

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,7 +4,13 @@ import numpy as np
 
 if __name__ == '__main__':
     data_path = os.path.abspath("../data/bunny_normalized.ply")
-    cloud = o3d.io.read_point_cloud(data_path) # Read the point cloud
-    
-    #o3d.visualization.draw_geometries([cloud], point_show_normal=True) # Visualize the point cloud
-
+    cloud_ply = o3d.io.read_point_cloud(data_path) # Read the point cloud
+    print(cloud_ply)
+    print(np.asarray(cloud_ply.points))
+    #o3d.visualization.draw_geometries([cloud_ply], point_show_normal=True) # Visualize the point cloud
+    mesh_obj = o3d.io.read_triangle_mesh("../data/shapenet_ex_2/model_normalized.obj")
+    cloud_obj = o3d.geometry.PointCloud()
+    cloud_obj.points = mesh_obj.vertices
+    cloud_obj.normals = mesh_obj.vertex_normals
+    cloud_obj.normalize_normals()
+    o3d.visualization.draw_geometries([cloud_obj])
